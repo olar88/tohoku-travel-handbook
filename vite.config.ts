f919e8b9c434b4daf548f172e88146ead64fdd8e
olar88 <parkeunyeon18@gmail.com>
2025-12-15 14:49:32 +0800

    feat: PWA setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,27 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: '東北旅遊手札',
+        short_name: '東北旅遊手札',
+        description: 'Your guide to traveling in Tohoku, Japan',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
